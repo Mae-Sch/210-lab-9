035f0763c484e52839d67db3861538415993db07
Mae-Sch <maeschw@proton.me>
2026-09-18 12:14:59 -0700

    update

--- 210-lab-9.cpp
+++ 210-lab-9.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
+
+
+
+int main() {
+    const int MAXSIZE = 50
+
+    // opens input file to read data
+    ifstream inputFile("groceryList.txt");
+    string groceryList[MAXSIZE];
+
+    // first line of file should be length of the list
+    int fileLength;
+    inputFile >> fileLength;
+    // fills groceryList with file contents
+    for (int i = 0; i < fileLength) {
+        
+    }
+
+
+    return 0;
+}
