1a98180589ee0dbc784bff0d919618b02ceab324
Mae-Sch <maeschw@proton.me>
2026-09-18 12:43:33 -0700

    non test output code

--- 210-lab-9.cpp
+++ 210-lab-9.cpp
@@ -6,10 +6,8 @@
 
 using namespace std;
 
-
-
 int main() {
-    const int MAXSIZE = 50;
+    const int MAXSIZE = 30;
 
     // opens input file to read data
     ifstream inputFile("groceryList.txt");
@@ -24,11 +22,17 @@ int main() {
     for (int i = 0; i < fileLength; ++i) {
         // collects grocery from line at stores it in the array
         getline(inputFile, groceryList.at(i));
-        // testing cout
-        cout << i << ": " << groceryList.at(i) << endl;
     }
     inputFile.close(); //file is no longer in use
 
+    // print array
+    cout << "list Contents:";
+    for (int i = 0; i < groceryList.size(); ++i) {
+        cout << (i+1) << ": " << groceryList.at(i) << " ";
+        if (i%3 == 0) // splits output into lines of 3 for readability
+            cout << endl;
+    }
+    cout << endl;
 
     return 0;
 }
