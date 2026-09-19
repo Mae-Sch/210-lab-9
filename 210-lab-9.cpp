47cbc3e267d9547146b7f1a7c953c1c372fbd354
Mae-Sch <maeschw@proton.me>
2026-09-18 19:45:35 -0700

    finished vector version

--- 210-lab-9.cpp
+++ 210-lab-9.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <array>
 #include <iomanip>
+#include <vector>
 
 using namespace std;
 
@@ -36,7 +37,7 @@ int main() {
     cout << endl << endl << endl;
 
     sort(groceryList.begin(), groceryList.end());
-    cout << "sorted list Contents:";
+    cout << "sorted list Contents:\n";
     for (int i = 0; i < groceryList.size(); ++i) {
         cout << setw(5) << (i+1) << ": " << setw(20) << groceryList.at(i) << " ";
         if (i%3 == 0) // splits output into lines of 3 for readability
@@ -51,6 +52,53 @@ int main() {
     array<string, MAXSIZE>::iterator foundIndex;
     foundIndex = find(groceryList.begin(), groceryList.end(), toFind);
     cout << toFind << " is at element " << (foundIndex - groceryList.begin() + 1) << endl;
+
+    // ********** Vector Version ********** //
+    // some variables have V appended to serpate from the array version
+
+    // opens input file to read data
+    ifstream inputFileV("groceryList.txt");
+    vector<string> groceryListV;
+
+    // first line of file should be length of the list
+    int fileLengthV;
+    inputFileV >> fileLengthV;
+    inputFileV.ignore(); // ignores rest of line used for list length
+
+    // fills groceryList with file contents
+    for (int i = 0; i < fileLengthV; ++i) {
+        // collects grocery from line at stores it in the array
+        string currentGrocery;
+        getline(inputFileV, currentGrocery);
+        groceryListV.push_back(currentGrocery);
+    }
+    inputFileV.close(); //file is no longer in use
+
+    // print array
+    cout << "list Contents:\n";
+    for (int i = 0; i < groceryListV.size(); ++i) {
+        cout << setw(5) << (i+1) << ": " << setw(20) << groceryListV.at(i) << " ";
+        if (i%3 == 0) // splits output into lines of 3 for readability
+            cout << endl;
+    }
+    cout << endl << endl << endl;
+
+    sort(groceryListV.begin(), groceryListV.end());
+    cout << "sorted list Contents:\n";
+    for (int i = 0; i < groceryListV.size(); ++i) {
+        cout << setw(5) << (i+1) << ": " << setw(20) << groceryListV.at(i) << " ";
+        if (i%3 == 0) // splits output into lines of 3 for readability
+            cout << endl;
+    }
+    cout << endl;
+
+    // using find() to return index
+    cout << "Enter grocery to find: ";
+    string toFindV;
+    cin >> toFindV;
+    vector<string>::iterator foundIndexV;
+    foundIndexV = find(groceryListV.begin(), groceryListV.end(), toFindV);
+    cout << toFindV << " is at element " << -1*(groceryListV.begin() - foundIndexV -1) << endl;
 
     return 0;
 }
